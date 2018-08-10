af3e812a4f6b2118ca88b36ce5aa4869e01ab87f
poojark66 <poojark66@gmail.com>
2018-08-10 17:18:38 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/create', function () {
     return view('blogs/create');
 });
 Route::post('/store', array('as' => 'store', 'uses' => 'BlogController@store'));
-Route::get('/index', array('as' => 'index', 'uses' => 'BlogController@index'));
-Route::get('/show/{id}', array('as' => 'show', 'uses' => 'BlogController@show'));
-Route::get('/edit/{id}', array('as' => 'edit', 'uses' => 'BlogController@edit'));
+Route::get('/index/{id}', array('as' => 'index', 'uses' => 'BlogController@index'));
+Route::get('/show/{id}/{uid}', array('as' => 'show', 'uses' => 'BlogController@show'));
+Route::get('/edit/{id}/{uid}', array('as' => 'edit', 'uses' => 'BlogController@edit'));
 Route::post('/update/{id}', array('as' => 'update', 'uses' => 'BlogController@update'));
